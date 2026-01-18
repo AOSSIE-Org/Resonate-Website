8c4e5dd54d7210e8acc00d46af0b60141aa29f0a
DeveloperAmrit <kittyamrit005@gmail.com>
2026-01-18 15:41:12 +0530

    Removed download from app store button

--- src/components/DownloadApp/DownloadApp.js
+++ src/components/DownloadApp/DownloadApp.js
@@ -12,10 +12,6 @@ const DownloadApp = () => {
             store="google" 
             url="https://play.google.com/store/apps/details?id=com.resonate.resonate" 
           />
-          <StoreButton 
-            store="apple" 
-            url="#" 
-          />
         </div>
       </div>
     </section>
