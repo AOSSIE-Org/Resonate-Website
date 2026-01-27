d0a10c0910430bf07181be5cf69ababcf94710b6
charul jain <charulj706@gmail.com>
2026-01-27 19:14:36 +0530

    Improve homepage SEO, content structure, and performance

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,6 @@ import './About.css';
 import aossieLogo from '../../assets/aossie_logo.png';
 import { FaEnvelope, FaGithub, FaDiscord } from 'react-icons/fa';
 import { BsTwitterX } from 'react-icons/bs';
-
 import { SiGitlab } from 'react-icons/si';
 
 const About = () => {
@@ -11,19 +10,26 @@ const About = () => {
     <section className="about">
       <div className="about-container">
         <div className="about-logo">
-          <img src={aossieLogo} alt="AOSSIE Logo" />
+          <img 
+            src={aossieLogo} 
+            alt="AOSSIE open source organization logo" 
+          />
         </div>
         <div className="about-content">
           <h2>We Innovate<br />We Educate</h2>
+
           <p>
-            We are an Australian not-for-profit umbrella organization for open-source projects. We believe the open-source philosophy provides a resource-efficient channel to transfer knowledge and achieve innovation and education.
+            AOSSIE is an Australian not-for-profit umbrella organization that supports and maintains open-source projects 
+            like Resonate. We believe the open-source philosophy provides a resource-efficient channel to transfer knowledge, 
+            empower communities, and drive innovation and education through collaborative software development.
           </p>
+
           <div className="social-links">
-            <a href="mailto:contact@aossie.org"><FaEnvelope /></a>
-            <a href="https://gitlab.com/aossie"><SiGitlab /></a>
-            <a href="https://github.com/AOSSIE-Org"><FaGithub /></a>
-            <a href="https://discord.com/invite/MMZBadkYFm"><FaDiscord /></a>
-            <a href="https://x.com/aossie_org"><BsTwitterX /></a>
+            <a href="mailto:contact@aossie.org" aria-label="Email AOSSIE"><FaEnvelope /></a>
+            <a href="https://gitlab.com/aossie" aria-label="AOSSIE GitLab"><SiGitlab /></a>
+            <a href="https://github.com/AOSSIE-Org" aria-label="AOSSIE GitHub"><FaGithub /></a>
+            <a href="https://discord.com/invite/MMZBadkYFm" aria-label="AOSSIE Discord"><FaDiscord /></a>
+            <a href="https://x.com/aossie_org" aria-label="AOSSIE Twitter/X"><BsTwitterX /></a>
           </div>
         </div>
       </div>
