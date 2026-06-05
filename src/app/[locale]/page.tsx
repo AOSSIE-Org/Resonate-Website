c32222c96840052ca650e5244637dbb25ca8da8b
Saksham Jain <113931310+reach2saksham@users.noreply.github.com>
2026-06-05 19:23:32 +0530

    fix: adhering all the components with respect to l10n requirements (#368)
    * fix: adhering all the components with respect to l10n requirements
    
    * fix: removing unused translation variable and duplicate margins from page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -12,16 +12,14 @@ import dynamic from 'next/dynamic';
 
 
 export default function Home() {
-  const t = useTranslations('Home');
   return (
     <div className="flex min-h-screen flex-col mx-4 sm:mx-8 lg:mx-16 xl:mx-48">
-      {/* Footer*/}
+      <div className="h-[300vh]">
+        <Hero />
+      </div>
       <div className="bottom-0 pb-4">
         <Footer />
       </div>
-    <div className="flex h-[400vh] flex-col mx-4 sm:mx-8 lg:mx-16 xl:mx-48 ">
-      <Hero />
-    </div>
    </div>
   );
 }
