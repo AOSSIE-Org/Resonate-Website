6e4cf9ebb9efb5fd9fbb785d3680100ce5272548
Amrit Kumar Yadav <105799602+DeveloperAmrit@users.noreply.github.com>
2026-06-18 15:45:08 +0530

    Refactor TechStack Component to Use SVG Icons and Text Labels (#266)
    * Replaced techstak images with actual components
    
    * Update dependabot.yml (#282)
    
    ---------
    
    Co-authored-by: Madhav Gupta <114633095+M4dhav@users.noreply.github.com>
    Co-authored-by: Aniket <iamaniketkumarmaner@gmail.com>

--- app/components/TechStack/TechStack.jsx
+++ app/components/TechStack/TechStack.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import "./TechStack.css";
-import flutterLogo from "../../assets/Flutter.png";
-import appwriteLogo from "../../assets/Appwrite.png";
 
 const TechStack = () => {
+  const technologies = [
+    { name: "Flutter", icon: "/flutter.svg" },
+    { name: "Appwrite", icon: "/appwrite.svg" },
+  ];
+
   return (
     <section className="tech-stack-container">
       <div className="tech-stack">
         <h2>TECH STACK</h2>
         <div className="tech-logos">
-          <div className="tech-logo">
-            <img src={flutterLogo.src} alt="Flutter"  draggable={false} />
-          </div>
-          <div className="tech-logo">
-            <img src={appwriteLogo.src} alt="Appwrite"  draggable={false} />
-          </div>
+          {technologies.map((tech) => (
+            <div key={tech.name} className="tech-logo">
+              <img src={tech.icon} alt={tech.name} className="tech-icon" />
+              <p className="tech-name">{tech.name}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
